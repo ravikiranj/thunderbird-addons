f519abb134a28734cad47faace1f1fe2ad671672
Ravikiran Janardhana <rjanardhana@rj-mac-dev.dhcp.tripadvisor.com>
2013-06-18 22:49:06 -0400

    Added debugging to console and shows images

--- ta-msg-header/content/overlay.js
+++ ta-msg-header/content/overlay.js
@@ -13,34 +13,52 @@ var tamsgheader = {
         }
     },
 
-    showMessageDetails: function(msg){
-        document.getElementById("tabmail").openTab("contentTab", {
-            contentPage: "about:blank",
-            onLoad: function (aEvent, aBrowser) {
-                var doc = aBrowser.contentDocument;
-                var pre = doc.createElement("pre");
-                pre.textContent = msg;
-                doc.body.appendChild(pre);
-            },
+    debugTab: function(msg) {
+       document.getElementById("tabmail").openTab("contentTab", {
+          contentPage: "about:blank",
+          onLoad: function (aEvent, aBrowser) {
+            let doc = aBrowser.contentDocument;
+            let pre = doc.createElement("pre");
+            pre.textContent = msg;; 
+            doc.body.appendChild(pre);
+          },
         });
     },
 
-    dumpObject: function(ob){
-        var op = '';
-        for(property in ob){
-            op += property + ': ' + ob[property]+';';
-        }
-        dump(op);
+    log: function(msg) {
+        var aConsoleService = Components.classes["@mozilla.org/consoleservice;1"].
+                              getService(Components.interfaces.nsIConsoleService);
+        aConsoleService.logStringMessage(msg);
     },
 
     onPageLoad: function(aEvent) {
-        var doc = aEvent.originalTarget; // doc is document that triggered "onload" event
-        dump(doc.body.innerHTML);
-        dump("NEW");
-        dump(doc.toSource());
-        dump("END");
-        
-        // add event listener for page unload 
+        var doc = aEvent.originalTarget;
+        var html = doc.body.innerHTML;
+        var msgHdr = gFolderDisplay.selectedMessage;
+        var hdrs;
+        MsgHdrToMimeMessage(msgHdr, null, function (aMsgHdr, aMimeMsg) {
+            hdrs = aMimeMsg.headers;
+            tamsgheader.log("Message Headers : \n" + aMimeMsg.headers.toSource());
+            var img = '<img src="https://lh5.googleusercontent.com/-8P5ax9d0gVc/AAAAAAAAAAI/AAAAAAAAAAA/qpUtADXVyvI/s27-c/photo.jpg" />';
+            img += '<p>From: ' + hdrs.from + ', User Status: unknown </p>';
+            doc.body.innerHTML = img + html;
+
+        });
+        tamsgheader.log("HTML : \n" + html);
+        var mPane = document.getElementById("messagepane");
+        tamsgheader.log("MPANE : \n" + mPane.toSource());
+//        var img = "<img src = 'https://lh5.googleusercontent.com/-8P5ax9d0gVc/AAAAAAAAAAI/AAAAAAAAAAA/qpUtADXVyvI/s27-c/photo.jpg' /> <p>From: " hdrs.from + " User Status: unknown</p>";
+
+        //All
+        /*
+        var all = document.getElementsByTagName("*");
+        int max = 10;
+        tamsgheader.log("ALL IDS:\n");
+        for(var i = 0; i < max && i < all.length; i++){
+            tamsgheader.log(all.id);
+        }
+        */
+        //Dont erase below
         aEvent.originalTarget.defaultView.addEventListener("unload", function(event){ tamsgheader.onPageUnload(event); }, true);
     },
 
